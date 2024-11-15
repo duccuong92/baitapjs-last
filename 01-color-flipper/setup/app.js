46c09eaec3a97c4d8ee8d575483438384bbfce7a
duccuong92 <cuahangbeeshop@gmail.com>
2024-11-15 19:12:07 +0700

    bai tap 1

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let changeColorBtn = document.querySelector(".simple-colors");
+let showColors = document.getElementById("show-colors");
+
+function changeColor() {
+  const randomColorsFunction = randomColors();
+  document.body.style.backgroundColor = colors[randomColorsFunction];
+  showColors.textContent = colors[randomColorsFunction];
+}
+
+function randomColors() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+changeColorBtn.onclick = changeColor;
